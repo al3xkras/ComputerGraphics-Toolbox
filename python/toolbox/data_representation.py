880784b0706e7ec94181f73f3bcb846cb0bb99bc
al3xkras <alexandr.krasovskiy@gmail.com>
2022-11-25 14:54:08 +0100

    Data representation: declare classes

---
+++ python/toolbox/data_representation.py
@@ -0,0 +1,42 @@
+class Point:
+    #todo implement
+    pass
+
+class Color:
+    #todo implement
+    pass
+
+class Segment:
+    #todo implement
+    pass
+
+class Vector:
+    #todo implement
+    pass
+
+class PointList:
+    #todo implement
+    pass
+
+class SegmentList:
+    #todo implement
+    pass
+
+class Polygon:
+    #todo implement
+    pass
+
+class PolygonVertexList:
+    #todo implement
+    pass
+
+class PolygonList:
+    #todo implement
+    pass
+
+#enum VectorSide
+class VectorSide:
+    LEFT=-1
+    RIGHT=1
+    NONE=0
+
